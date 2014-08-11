5378cdff5a9e31090064514fec185359e42f0856
Eddú Meléndez Gonzales <eddu.melendez@gmail.com>
2014-08-11 03:37:27 -0500

    adding scripts

---
+++ scripts/1.0.0/001_create_user_table.sql
@@ -0,0 +1,6 @@
+create table t_user (
+	id INTEGER ,
+	username VARCHAR(50),
+	password VARCHAR(50),
+	fullname VARCHAR(50)
+)
---
+++ scripts/1.1.0/001_create_role_table.sql
@@ -0,0 +1,4 @@
+create table t_role (
+	id INTEGER ,
+	description VARCHAR(50)
+)
